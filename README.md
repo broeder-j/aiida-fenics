a12f2ff9d77673211d13527de87e4312b6162a60
Sebastiaan Huber <mail@sphuber.net>
2021-12-06 11:17:00 +0100

    Initial commit
    Skeleton AiiDA plugin package.

---
+++ README.md
@@ -0,0 +1,3 @@
+# `aiida-fenics`
+
+AiiDA plugin for the [FEniCS computing platform](https://fenicsproject.org/) for solving partial differential equations.
---
+++ aiida_fenics/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""AiiDA plugin for the FEniCS computing platform for solving partial differential equations."""
+__version__ = '0.1.0'
